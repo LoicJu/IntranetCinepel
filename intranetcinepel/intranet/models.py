858d3786c3994e852da8341e9d9590cbce5c0ec2
Loic Jurasz <loic.jurasz@he-arc.ch>
2020-05-15 10:47:29 +0200

    update journal de bord

--- intranetcinepel/intranet/models.py
+++ intranetcinepel/intranet/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class NewUserModel(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        posts = models.IntegerField(default=0)
--- intranetcinepel/intranetcinepel/wsgi.py
+++ intranetcinepel/intranetcinepel/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intranetcinepel.local_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intranetcinepel.settings.local_settings')
 
 application = get_wsgi_application()
