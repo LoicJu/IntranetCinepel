dbcc1e40b9c89b0af5fade98f98a0b61486fac50
Loic Jurasz <loic.jurasz@he-arc.ch>
2020-05-22 10:58:49 +0200

    create migrations for users

--- intranetcinepel/intranetcinepel/settings/common.py
+++ intranetcinepel/intranetcinepel/settings/common.py
@@ -33,6 +33,10 @@ INSTALLED_APPS = [
     'knox',
     ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
