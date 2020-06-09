81c6c343b78fea6439320acb1db5d030b0bdb04d
Loic Jurasz <loic.jurasz@he-arc.ch>
2020-06-09 14:04:58 +0200

    can get from select and delete in template

--- intranetcinepel/intranet_frontend_app/src/components/TemplatePlanning.js
+++ intranetcinepel/intranet_frontend_app/src/components/TemplatePlanning.js
@@ -5,6 +5,7 @@ import Error from './Error';
 import axios from 'axios';
 import Table from "./Table";
 import {AppProvider, Page} from '@shopify/polaris';
+import Select from 'react-select'
 
 class TemplatePlanning extends Component {
   static contextType = AuthContext
@@ -12,27 +13,59 @@ class TemplatePlanning extends Component {
     super(props);
     this.state={
       namePost : '',
+      nameDel : '',
       nameTemplate : '',
+      nameIdTemplate : {},
+      nameAllTemplate : [],
       columns : [],
       data : [],
       is_created : false,
       error : null,
       is_delete : null,
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangePost = this.handleChangePost.bind(this);
+    this.handleChangeGet = this.handleChangeGet.bind(this);
+    this.handleChangeDel = this.handleChangeDel.bind(this);
     this.submitTemplate = this.submitTemplate.bind(this);
-    this.getTemplate = this.getTemplate.bind(this);
     this.deleteTemplate = this.deleteTemplate.bind(this);
   };
-  
-  handleChange(event){    
+
+  handleChangeState(){
+    axios({
+      method: 'get',
+      url: 'api/template/',
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        this.setState({
+          nameAllTemplate: [],
+          nameIdTemplate : {},
+        });
+        Object.keys(response.data).forEach(key => this.state.nameAllTemplate.push({"value" : response.data[key].name, "label" : response.data[key].name}));
+        Object.keys(response.data).forEach(key => this.state.nameIdTemplate[response.data[key].name] = response.data[key].id);
+      }
+    })
+    .catch((error) => {
+      if(error.response) {
+        this.setState({
+          error: {
+            status: error.response.status + ' ' + error.response.statusText,
+            detail: error.response.data.detail,
+          }
+        });
+      }
+    });
+
+  }
+
+  handleChangePost(event){    
     this.setState({namePost: event.target.value});
   }
+
   // store a new template
   submitTemplate(event) {
     event.preventDefault();
 
-    // TODO store elsewhere than session ? see security
     let authed_user = sessionStorage.getItem('authed_user');
 
     var templateFormData = new FormData();
@@ -44,7 +77,8 @@ class TemplatePlanning extends Component {
       data: templateFormData,
     })
     .then((response) => {
-      if (response.status === 200) {
+      if (response.status === 201) {
+        this.handleChangeState();
         this.setState({ is_created: true });
       }
     })
@@ -60,23 +94,22 @@ class TemplatePlanning extends Component {
     });
   }
 
-  // TODO get in function of a name (scrolllist where there is all the name of the templates) and so get by the id
-  getTemplate(){
+  // handle change and get 
+  handleChangeGet(event){
+    let id = this.state.nameIdTemplate[event.value]
     axios({
       method: 'get',
-      url: 'api/template/15',
+      url: 'api/template/' + id,
       responseType: 'json',
     })
     .then((response) => {
       if (response.status === 200) {
-        const arrCol = [];
-        const arrData = [];
-        Object.keys(response.data.columns).forEach(key => arrCol.push(response.data.columns[key]));
-        Object.keys(response.data.content).forEach(key => arrData.push(response.data.content[key]));
+        this.state.columns = [];
+        this.state.data = [];
+        Object.keys(response.data.columns).forEach(key => this.state.columns.push(response.data.columns[key]));
+        Object.keys(response.data.content).forEach(key => this.state.data.push(response.data.content[key]));
         this.setState({
           nameTemplate : response.data.name,
-          columns : arrCol,
-          data : arrData,
         });
       }
     })
@@ -92,13 +125,20 @@ class TemplatePlanning extends Component {
     });
   }
   
+  handleChangeDel(event){
+    this.setState({nameDel: event.value});
+  }
+
+  // delete a template
   deleteTemplate(){
+    let id = this.state.nameIdTemplate[this.state.nameDel]
     axios({
       method: 'delete',
-      url: 'api/template/1',
+      url: 'api/template/' + id,
     })
     .then((response) => {
-      if (response.status === 200) {
+      if (response.status === 204) {
+        this.handleChangeState()
         this.setState({
           is_delete: true,
         });
@@ -117,7 +157,26 @@ class TemplatePlanning extends Component {
   }
   
   componentDidMount(){
-    
+    axios({
+      method: 'get',
+      url: 'api/template/',
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        Object.keys(response.data).forEach(key => this.state.nameAllTemplate.push({"value" : response.data[key].name, "label" : response.data[key].name}));
+        Object.keys(response.data).forEach(key => this.state.nameIdTemplate[response.data[key].name] = response.data[key].id);
+      }
+    })
+    .catch((error) => {
+      if(error.response) {
+        this.setState({
+          error: {
+            status: error.response.status + ' ' + error.response.statusText,
+            detail: error.response.data.detail,
+          }
+        });
+      }
+    });
   }
 
   render(){
@@ -138,20 +197,26 @@ class TemplatePlanning extends Component {
                 type="text"
                 className="form-control"
                 value={this.state.name}
-                onChange={this.handleChange}
+                onChange={this.handleChangePost}
                 />
             </div>
             <div className="form-group">
-              <button type="submit" className="btn btn-danger">post</button>
+              <button type="submit" className="btn btn-danger">créer</button>
             </div>
           </form>
-          <button onClick={this.getTemplate}>get</button>
-          <div>
-            <h4>{this.state.nameTemplate}</h4>
-          </div>
-          <button onClick={this.deleteTemplate}>del</button>
+          <Select 
+            placeholder="Choisissez le template à supprimer"
+            onChange={this.handleChangeDel}
+            options={this.state.nameAllTemplate}
+          />
+          <button onClick={this.deleteTemplate}>supprimer</button>
+          <Select 
+            placeholder="Choisissez le template"
+            onChange={this.handleChangeGet}
+            options={this.state.nameAllTemplate}
+         />
           <AppProvider>
-            <Page title="Data table">
+            <Page title={this.state.nameTemplate}>
               <Table headings={this.state.columns} rows={this.state.data} />
             </Page>
           </AppProvider>
